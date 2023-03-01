20a3776b82665319594c9a836b423b1dae61f63a
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-03-01 14:37:01 +0200

    logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -4,25 +4,17 @@ import './Logo.scss';
 import logo from '../../static/Logo.svg';
 
 interface Props {
-  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>,
+  setIsOpen?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const Logo: React.FC<Props> = memo(({ setIsOpen }) => {
-  return setIsOpen
-    ? (
-      <Link
-        className="logo"
-        to="/"
-        onClick={() => setIsOpen(false)}
-      >
-        <img src={logo} className="logo_image" alt="NiceGadgets logo" />
-      </Link>
-    ) : (
-      <Link
-        className="logo"
-        to="/"
-      >
-        <img src={logo} className="logo__image" alt="NiceGadgets logo" />
-      </Link>
-    );
+  return setIsOpen ? (
+    <Link className="logo" to="/" onClick={() => setIsOpen(false)}>
+      <img src={logo} className="logo_image" alt="NiceGadgets logo" />
+    </Link>
+  ) : (
+    <Link className="logo" to="/">
+      <img src={logo} className="logo__image" alt="NiceGadgets logo" />
+    </Link>
+  );
 });
