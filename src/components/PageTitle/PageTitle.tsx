eda2451401baa0cb69b1d8e485f009833f17eeca
Kyrylo Haiduk <49764881+superpooperxxx@users.noreply.github.com>
2023-03-01 21:36:47 +0200

    added memo to PageTitle and changed to h2

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { memo } from 'react';
 import './PageTitle.scss';
 
 type Props = {
   children: string;
 };
 
-export const PageTitle: React.FC<Props> = ({ children }) => (
-  <h1 className="page-title">{children}</h1>
-);
+export const PageTitle: React.FC<Props> = memo(({ children }) => (
+  <h2 className="page-title">{children}</h2>
+));
