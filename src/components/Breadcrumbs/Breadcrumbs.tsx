48681c9c77c01466e748ebe628d654df24710161
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-03-04 20:48:37 +0200

    added breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -9,7 +9,7 @@ export const Breadcrumbs: React.FC = memo(() => {
 
   const crumbs = location.pathname
     .split('/')
-    .filter(crumb => crumb !== '')
+    .filter((crumb) => crumb !== '')
     .map((crumb) => {
       const preparedCrumb = crumb[0].toUpperCase() + crumb.slice(1);
 
@@ -17,7 +17,7 @@ export const Breadcrumbs: React.FC = memo(() => {
 
       return (
         <Fragment key={crumb}>
-          <div className="breadcrumbs__arrow-icon"></div>
+          <div className="breadcrumbs__icon breadcrumbs__icon--arrow"></div>
           <Link
             aria-label={`click to go to the ${crumb} page`}
             className="breadcrumbs__link"
@@ -37,7 +37,7 @@ export const Breadcrumbs: React.FC = memo(() => {
         to="/"
         className="breadcrumbs__link"
       >
-        <div className="breadcrumbs__home-icon"></div>
+        <div className="breadcrumbs__icon--home breadcrumbs__icon"></div>
       </Link>
       <>{crumbs}</>
     </div>
