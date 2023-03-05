83c5c9d7ee507ebef924e98397af1aab3979e0cc
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-03-05 17:25:58 +0200

    not-found-page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -10,16 +10,24 @@ export const NotFoundPage: React.FC = () => {
       <BackButton isAlone />
       <section className="not-found-page__content">
         <div className="not-found-page__text">
-          <h1 className="not-found-page__title">Sorry, this page could not be found</h1>
+          <h1 className="not-found-page__title">
+            Sorry, this page could not be found
+          </h1>
           <p className="not-found-page__description">
             {'The link is broken or the page no longer exists. '}
             <br className="not-found-page__break" />
             {'Go to the '}
-            <Link className="not-found-page__link" to="/">homepage</Link>
+            <Link className="not-found-page__link" to="/">
+              homepage
+            </Link>
             .
           </p>
         </div>
-        <img src={errorImage} alt="404 error" className="not-found-page__image" />
+        <img
+          src={errorImage}
+          alt="404 error"
+          className="not-found-page__image"
+        />
       </section>
     </div>
   );
