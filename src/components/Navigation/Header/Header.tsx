f794620fae0b643a6625f9b15b4cba453951c9b9
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2023-03-07 15:24:35 +0200

    context

--- src/components/Navigation/Header/Header.tsx
+++ src/components/Navigation/Header/Header.tsx
@@ -1,6 +1,4 @@
-import React, {
-  useCallback, useEffect, useState,
-} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import cn from 'classnames';
 import './Header.scss';
 import { Nav } from '../Nav/Nav';
